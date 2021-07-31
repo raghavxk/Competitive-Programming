24e1b78bcf9e39f72ddab648b1c5a9d4326df8ea
Raghavendra Khare <raghavxk@gmail.com>
2021-07-31 19:27:24 +0530

    (A) PizzaForces.cpp

---
+++ codeforces/Educational Codeforces Round 112 (Rated for Div. 2)/(A) PizzaForces.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#include <chrono>
+#include <ctime>
+
+#define deb(x) cout << (#x) << " is " << (x) << endl
+#define nline "\n"
+#define read(x) int x; cin>>x
+#define SaveTime() ios_base::sync_with_stdio(false);std::cin.tie(NULL)
+typedef long long int ll;
+
+const int MOD = 1000000007;
+
+using namespace std;
+
+int solve() {
+    ll n, out = 0;
+    cin >> n;
+    if (n < 6)
+        cout << 15 << nline;
+    else {
+        cout<<((n+1)/2)*5<<nline;
+    }
+    return 0;
+}
+
+int main() {
+    SaveTime();
+#ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+    auto start1 = std::chrono::high_resolution_clock::now();
+#endif
+
+    int T = 1;
+    cin >> T;
+    while (T--)
+        solve();
+
+    return 0;
+}
