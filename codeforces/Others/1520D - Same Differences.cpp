8d3796be9dca07bcea08ce1baae8c018502a6397
Raghavendra Khare <raghavxk@gmail.com>
2021-08-04 14:25:42 +0530

    efforts

---
+++ codeforces/Others/1520D - Same Differences.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#include <chrono>
+#include <ctime>
+
+#define deb(x) cout << (#x) << " is " << (x) << endl
+#define nline "\n"
+#define read(x) int x; cin>>x
+#define SaveTime() ios_base::sync_with_stdio(false);std::cin.tie(NULL)
+typedef long long int ll;
+
+const int MOD = 1000000007;
+
+using namespace std;
+
+void printVect(vector<int> vi, int size) {
+    for (ll i = 0; i < size; ++i) {
+        cout << vi[i] << " ";
+    }
+    cout << nline;
+}
+
+
+int solve() {
+    int n, x;
+    ll ans = 0;
+    cin >> n;
+    map<int, int> m;
+    for (int i = 0; i < n; ++i) {
+        cin >> x;
+        x = x - i;
+        ans += m[x];
+        m[x]++;
+    }
+    cout << ans << nline;
+    
+    return 0;
+}
+
+int main() {
+    SaveTime();
+#ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+    auto start1 = std::chrono::high_resolution_clock::now();
+#endif
+
+    int T = 1;
+    cin >> T;
+    while (T--)
+        solve();
+
+    return 0;
+}
