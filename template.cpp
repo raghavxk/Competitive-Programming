3462d8e339e4bb989cb314d9c943e52015f975f8
Raghavendra Khare <raghavxk@gmail.com>
2021-08-22 23:07:45 +0530

    updates to template

--- template.cpp
+++ template.cpp
@@ -1,38 +1,42 @@
 #include <bits/stdc++.h>
-#include <chrono>
-#include <ctime>
 
 #define deb(x) cout << (#x) << " is " << (x) << endl
 #define nline "\n"
 #define read(x) int x; cin>>x
-#define fastio() ios_base::sync_with_stdio(false);std::cin.tie(NULL)
-typedef long long int ll;
+#define SaveTime() ios_base::sync_with_stdio(false);std::cin.tie(NULL)
+#define setDecimalPrecision(Precision) fixed<<showpoint<<setprecision(Precision)
+typedef long long ll;
+
+const int MOD = 1000000007;
+const float PI = 3.14159265;
 
 using namespace std;
 
+void PrintVect(vector<int> vi) {
+    ll size = vi.size();
+    for (ll i = 0; i < size; ++i) {
+        cout << vi[i] << " ";
+    }
+    cout << nline;
+    return;
+}
+
 int solve() {
-  
+
     return 0;
 }
 
 int main() {
-    fastio();
+    SaveTime();
 #ifndef ONLINE_JUDGE
     freopen("Input.txt", "r", stdin);
     freopen("Output.txt", "w", stdout);
-    auto start1 = std::chrono::high_resolution_clock::now();
 #endif
 
-    int t = 1;
-    cin >> t;
-    while (t--)
+    int T = 1;
+    cin >> T;
+    while (T--)
         solve();
-
-#ifndef ONLINE_JUDGE
-    auto stop1 = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop1 - start1);
-    cerr << "Time : " << duration.count() / 1000000 << "seconds" << endl;
-#endif
 
     return 0;
 }
