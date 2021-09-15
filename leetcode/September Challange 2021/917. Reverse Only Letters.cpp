4ea83c440d37ba76a6afa5d19ce28b85c79c92b9
Raghavendra Khare <raghavxk@gmail.com>
2021-09-15 09:54:13 +0530

    code inserted

--- leetcode/September Challange 2021/917. Reverse Only Letters.cpp
+++ leetcode/September Challange 2021/917. Reverse Only Letters.cpp
@@ -1,1 +1,19 @@
-.
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int len = s.length();
+        int left =0,right = len-1;
+        while(left<right){
+            while(left<right and !isalpha(s[left])){
+                left++;
+            }
+            while(left < right and !isalpha(s[right])){
+                right--;
+            }
+            swap(s[left],s[right]);
+            left++;
+            right--;
+        }
+        return s;
+    }
+};
