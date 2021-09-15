45f7ebd030bce435172f3d7eca91744c01b50ba5
Raghavendra Khare <raghavxk@gmail.com>
2021-09-15 23:22:39 +0530

    code updated

--- codeforces/Others/1520D - Same Differences.cpp
+++ codeforces/Others/1520D - Same Differences.cpp
@@ -1,53 +1,56 @@
 #include <bits/stdc++.h>
-#include <chrono>
-#include <ctime>
 
 #define deb(x) cout << (#x) << " is " << (x) << endl
-#define nline "\n"
-#define read(x) int x; cin>>x
-#define SaveTime() ios_base::sync_with_stdio(false);std::cin.tie(NULL)
+#define endl "\n"
+#define SaveTime()                    \
+    ios_base::sync_with_stdio(false); \
+    std::cin.tie(NULL)
+#define setDecimalPrecision(Precision) fixed << showpoint << setprecision(Precision)
 typedef long long int ll;
 
 const int MOD = 1000000007;
+const float PI = 3.14159265;
 
 using namespace std;
 
-void printVect(vector<int> vi, int size) {
-    for (ll i = 0; i < size; ++i) {
-        cout << vi[i] << " ";
+// Code starts here !
+
+void solve()
+{
+    ll tc;
+    cin >> tc;
+    while (tc--)
+    {
+        ll size;
+        cin >> size;
+        map<ll, ll> mp;
+        ll inp;
+        for (ll i = 1; i < size+1; ++i)
+        {
+            cin >> inp;
+            inp = inp - i;
+            mp[inp]++;
+        }
+        ll res = 0;
+        map<ll,ll>::iterator itr;
+        for(itr=mp.begin();itr!=mp.end();++itr)
+        {
+            // cout<<itr->first<<" "<<itr->second<<endl;
+            res += (itr->second * (itr->second-1))/2; 
+        }
+        cout<<res<<endl;
     }
-    cout << nline;
 }
 
-
-int solve() {
-    int n, x;
-    ll ans = 0;
-    cin >> n;
-    map<int, int> m;
-    for (int i = 0; i < n; ++i) {
-        cin >> x;
-        x = x - i;
-        ans += m[x];
-        m[x]++;
-    }
-    cout << ans << nline;
-    
-    return 0;
-}
-
-int main() {
+signed main()
+{
     SaveTime();
 #ifndef ONLINE_JUDGE
     freopen("Input.txt", "r", stdin);
     freopen("Output.txt", "w", stdout);
-    auto start1 = std::chrono::high_resolution_clock::now();
 #endif
 
-    int T = 1;
-    cin >> T;
-    while (T--)
-        solve();
+    solve();
 
     return 0;
 }
